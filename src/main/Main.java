8417e1b02a2da9216d0298a9f17652edc6e881be
Muhammed Emre Eğer <m.emre.eger@gmail.com>
2026-04-07 04:09:05 +0300

    refactor : delete comment messages

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,6 @@ import manager.HospitalManager;
 
 public class Main {
     public static void main(String[] args) {
-        //MenuManager menuManager = new MenuManager();
-        //menuManager.start();
 
         HospitalManager hospitalManager = new HospitalManager();
         hospitalManager.begin();
